c8d7242b7919237985bad4cc781383701c4676fa
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-08-10 18:16:04 -0300

    create SearchParams component with hooks

---
+++ main-courses/01-intro/src/SearchParams.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { ANIMALS } from '@frontendmasters/pet'
+
+const SearchParams = () => {
+    const [location, setLocation] = useState('Seattle, WA')
+    const [animal, setAnimal] = useState('Dog')
+
+    return (
+        <div className="search-params">
+            <form>
+                <label htmlFor="location">
+                    Location
+                    <input
+                        id="location"
+                        value={location}
+                        onChange={
+                            (e) => setLocation(e.target.value)
+                        }
+                        placeholder="Location"
+                    />
+                </label>
+                <label htmlFor="Animal">
+                    Animal
+                    <select
+                        id="animal"
+                        value={animal}
+                        onChange={
+                            (e) => setAnimal(e.target.value)
+                        }
+                        onBlur={
+                            (e) => setAnimal(e.target.value)
+                        }
+                        placeholder="animal"
+                    >
+                        <option>All</option>
+                        {ANIMALS.map(animal => (
+                            <option value={animal}>{animal}</option>
+                        ))}
+
+                    </select>
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default SearchParams
