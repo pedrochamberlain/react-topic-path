c7c4a776d3a610e72ad3f341402b55f4f90ccfbc
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-08-11 17:17:55 -0300

    create components folder

--- main-courses/01-intro/src/App.js
+++ main-courses/01-intro/src/App.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Link } from '@reach/router'
 
-import SearchParams from './SearchParams'
-import Details from './Details'
+import SearchParams from './components/SearchParams'
+import Details from './components/Details'
 
 const App = () => {
     return (
