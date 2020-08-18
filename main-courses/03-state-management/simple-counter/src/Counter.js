faade67d7a73046e4870a0eb7149670b39c55b24
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-08-17 21:26:09 -0300

    add basic increment, decrement and reset functions

--- main-courses/03-state-management/simple-counter/src/Counter.js
+++ main-courses/03-state-management/simple-counter/src/Counter.js
@@ -1,14 +1,40 @@
 import React from 'react'
 
 class Counter extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            count: 0
+        }
+
+        this.increment = this.increment.bind(this)
+        this.decrement = this.decrement.bind(this)
+        this.reset = this.reset.bind(this)
+    }
+
+    increment() {
+        this.setState({ count: this.state.count + 1 })
+    }
+
+    decrement() {
+        this.setState({ count: this.state.count - 1 })
+    }
+
+    reset() {
+        this.setState({ count: 0 })
+    }
+
     render() {
+        const { count } = this.state
+
         return (
             <main className="Counter">
-                <p className="count">0</p>
+                <p className="count">{this.state.count}</p>
                 <section className="controls">
-                    <button>Increment</button>
-                    <button>Decrement</button>
-                    <button>Reset</button>
+                    <button onClick={this.increment}>Increment</button>
+                    <button onClick={this.decrement}>Decrement</button>
+                    <button onClick={this.reset}>Reset</button>
                 </section>
             </main>
         );
