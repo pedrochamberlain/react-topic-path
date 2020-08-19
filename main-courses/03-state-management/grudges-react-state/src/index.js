daeb49762a18308d9916e9a09a0b20094b5e2b94
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-08-18 23:11:45 -0300

    import grudge list project

---
+++ main-courses/03-state-management/grudges-react-state/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Application from './Application';
+
+import './style.scss';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<Application />, rootElement);
